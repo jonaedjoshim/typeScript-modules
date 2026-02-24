68120e32a902c229f5f4969e835cbfe62ce40f4d
mdjonaedjoshim <mdjonaedjoshim@gmail.com>
2026-02-24 22:36:44 +0600

    learned how the object works in ts

--- module-1/src/1_5.ts
+++ module-1/src/1_5.ts
@@ -5,7 +5,8 @@ const user: {
     firstName: string;
     middleName?: string; // optional property, we can make it optional by adding '?' after the property name like this: middleName?: string. That means if user donen't provide a value for middleName, it will be undefined and there will be no error. but if he provide a value for middleName, it must be a string, otherwise there will be an error.
     lastName: string;
-    age: number;
+    // age: number;
+    readonly age: number;
     isMarried: boolean;
 } = {
     // learningFrom: 'Programming Hero Bangladesh', // Error: Type '"Programming Hero Bangladesh"' is not assignable to type '"Programming Hero"'.
@@ -16,3 +17,6 @@ const user: {
     age: 22,
     isMarried: true
 }
+
+// readonly property means that we can only assign a value to it once, and we can't change it afterwards. So if we try to change the value of 'age' property, we will get an error:
+// user.age = 23 // Error: Cannot assign to 'age' because it is a read-only property.
