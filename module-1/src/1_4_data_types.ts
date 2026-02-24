06b9a7e1b3f8ffc5bae6a63fed9dcc349796b14d
mdjonaedjoshim <mdjonaedjoshim@gmail.com>
2026-02-24 22:02:56 +0600

    learning data types of ts

---
+++ module-1/src/1_4_data_types.ts
@@ -0,0 +1,16 @@
+// Basic Data Types in TypeScript
+
+// 1. String
+let Name : string = 'Jonaed'
+
+// 2. Number
+let age : number = 22
+
+// 3. Boolean
+let isAdmin : boolean = true
+
+// 4. undefined
+let x : undefined = undefined
+
+// 5. null
+let y : null = null
